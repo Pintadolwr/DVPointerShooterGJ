cdd9715a38d3cd319c089928af62d08068efeb75
Pintadolwr <miguel.pintado20@estudantes.ips.pt>
2022-05-21 15:55:07 +0100

    Score, timer e gameover

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
     private Rigidbody rb;
+
     [SerializeField]
     private float speed = 30.0f;
+
+    private int score = 0;
+
+    [SerializeField]
+    public Text scoreText;
+
+    [SerializeField]
+    public Text timeText;
+
+    [SerializeField]
+    public Text gameOverText;
+
+    [SerializeField]
+    public float timeCount = 30;
+
+    [SerializeField]
+    public int maxScore;
 
     private Vector3 snapPosition;
     private Quaternion originalRotationValue;
@@ -16,6 +35,8 @@ public class Target : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         snapPosition = this.transform.position;
         originalRotationValue = transform.rotation;
+        scoreText.text = "Score: " + score;
+        gameOverText.text = "";
     }
 
     // Update is called once per frame
@@ -26,6 +47,32 @@ public class Target : MonoBehaviour
             transform.rotation = originalRotationValue;
             rb.angularVelocity = new Vector3(0, 0, 0);
             rb.velocity = new Vector3(0, 0, 0);
+        }
+        timeCounter();
+        scoreCounter();
+    }
+
+    void timeCounter() { 
+        if(timeCount > 0)
+        {
+            timeCount -= 1 * Time.deltaTime;
+        }
+        else
+        {
+            gameOverText.text = "GAME OVER!";
+            timeCount = 0;
+            Time.timeScale = 0f;
+        }
+        timeText.text = "Time: " + timeCount;
+    }
+
+    void scoreCounter() {
+        if (score == maxScore)
+        {
+            gameOverText.text = "GAME OVER!";
+            timeCount = 0;
+            Time.timeScale = 0f;
+            timeText.text = "Time: " + timeCount;
         }
     }
 
@@ -41,6 +88,10 @@ public class Target : MonoBehaviour
             this.speed = speed * -1;
             Vector3 movement = new Vector3(5, 0, 0);
             rb.AddForce(movement * speed);
+        }else if (collision.collider.CompareTag("bullet"))
+        {
+            score++;
+            scoreText.text = "Score: " + score;
         }
     }
 }
